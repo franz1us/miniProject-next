7f4ce3b2472df46fa208823ccbb436cb2f3d35a6
franz1us <franzius.won@gmail.com>
2024-08-17 23:45:45 +0700

    add mongoDB
    add mongoDB

--- nextjs-auth/src/app/api/register/route.js
+++ nextjs-auth/src/app/api/register/route.js
@@ -1,8 +1,15 @@
 import { NextResponse } from "next/server";
+import { connectMongoDB } from "../../../../lib/mongdb";
+import User from "../../../../models/user";
+import bcrypt from "bcryptjs"
 
 export async function POST(req) {
     try{
             const {username, email,password} = await req.json();
+            const hashPassword = await bcrypt.hash(password,10);
+
+            await connectMongoDB();
+            await User.create({username, email, password: hashPassword});
 
             console.log('Username: ',username);
             console.log('Email: ',email);
